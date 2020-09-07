5828b98741422d7fbddacc2eec7d11746f6ee549
MTATrooper <long96nb@gmail.com>
2020-09-07 15:34:27 +0700

    07.9.2020.15.33

--- test.py
+++ test.py
@@ -1,18 +1,5 @@
-import _thread
-import time
+from news_crawler.spiders.dantri import CATEGORIES as dantri_categories
 
-# Define a function for the thread
-def print_time( threadName, delay):
-   count = 0
-   while count < 5:
-      time.sleep(delay)
-      count += 1
-      print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+pages_1 = {'dantri': dantri_categories}
 
-# Create two threads as follows
-try:
-   _thread.start_new_thread( print_time, ("Thread-1", 1, ) )
-   _thread.start_new_thread( print_time, ("Thread-2", 2, ) )
-except:
-   print ("Error: unable to start thread")
-
+print(list(pages_1.keys())[0])
